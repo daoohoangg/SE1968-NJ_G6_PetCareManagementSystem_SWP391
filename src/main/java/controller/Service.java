bbf9ef411b41a67164963618fb0707db790cfec2
Sonha0511 <thientaim18@gmail.com>
2025-09-30 00:35:58 +0700

    test commit

--- src/main/java/controller/Service.java
+++ src/main/java/controller/Service.java
@@ -3,4 +3,8 @@ package controller;
 public class Service {
     private String name;
     private String email;
+    private String password;
+    public Service(String name, String email, String password) {
+        this.name = name;
+    }
 }
