af058bec79a19080f75986cb0d9d49127edc819c
Hoang Trung Dao <64149095+daoohoangg@users.noreply.github.com>
2025-09-30 22:39:59 +0700

    Service Managemnet

---
+++ src/main/java/com/petcaresystem/controller/admin/ServiceController.java
@@ -0,0 +1,18 @@
+package com.petcaresystem.controller.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/serivce")
+public class ServiceController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/adminpage/dashbroad.jsp").forward(req, resp);
+    }
+}
